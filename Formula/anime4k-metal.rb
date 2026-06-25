e39b2cb02e2441fc4c653983257e98ae94797236
xzhih <triton2you@gmail.com>
2026-06-25 19:05:07 +0800

    chore: update anime4k-metal formula for v0.1.2

--- Formula/anime4k-metal.rb
+++ Formula/anime4k-metal.rb
@@ -1,17 +1,17 @@
 class Anime4kMetal < Formula
   desc "Native macOS Anime4K image enhancement CLI"
   homepage "https://github.com/cinemore/anime4k-metal"
-  version "0.1.1"
+  version "0.1.2"
   license "MIT"
 
   depends_on macos: :ventura
 
   if Hardware::CPU.arm?
-    url "https://github.com/cinemore/anime4k-metal/releases/download/v0.1.1/anime4k-metal-macos-arm64.tar.gz"
-    sha256 "b5269f4416559e9a0cdb2340f9f43a6301dd162ef3a2b742debac060c47edb31"
+    url "https://github.com/cinemore/anime4k-metal/releases/download/v0.1.2/anime4k-metal-macos-arm64.tar.gz"
+    sha256 "ab9cefa2920663adbcee0804cc42ed0325888d12a7a9b35ae59b508bd0f91929"
   else
-    url "https://github.com/cinemore/anime4k-metal/releases/download/v0.1.1/anime4k-metal-macos-x86_64.tar.gz"
-    sha256 "646aa805906e9ce8172667ee05c4fbde0e22129595daff514bfed0ac30390b1c"
+    url "https://github.com/cinemore/anime4k-metal/releases/download/v0.1.2/anime4k-metal-macos-x86_64.tar.gz"
+    sha256 "3cb0a475bc0282d0611d8620b805bce3dda2f0f9e12db57623212b1fa38abd57"
   end
 
   def install
